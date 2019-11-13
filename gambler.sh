6178855ac75a2aac0b18cf2b9c0eb6a49eda571b
Akshayrp <akshaypatwari24@gmail.com>
2019-11-13 11:19:35 +0530

    gambler bet of   to either win or loss

--- gambler.sh
+++ gambler.sh
@@ -5,21 +5,39 @@ echo "----------GamblerGame----------"
 read -p "Enter how many days you want to play" days
 read -p "how many Games in day ? " games
 declare -A stakelist
-dayss
+dayss=0;
 stakes=0;
+win=1;
+
+function gamble()
+{
+	bet=$(( RANDOM % 2 ))
+	if [ $bet -eq $win ]
+		then
+		stake=$(( $stake + 1 ))
+		else
+		stake=$(( $stake - 1 ))
+	fi
+}
 
 for((day=1; day <= $days; day++))
 do
+	echo "---day" $day "---"
 	stake=100;
 	
 	for((game=1; game <= $games; game++))
 	do
-	stake=$(($stake-1))
+	gamble $stake
 	echo $stake	
 	done
 
 
-
 stakes=$(($stakes+$stake))
-
+# COMPUTING ALL DAYS FINAL STAKES INTO A DICTIONARY
+#stakelist[$((dayss++))]=$stake
 done
+#echo ${stakelist[@]}
+
+
+
+
