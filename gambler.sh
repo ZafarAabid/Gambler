b0afab524db12a809850fdbff2a11f0ecc4ce25c
Akshayrp <akshaypatwari24@gmail.com>
2019-11-13 10:54:08 +0530

    added functionality of stake and bet

--- gambler.sh
+++ gambler.sh
@@ -1,4 +1,25 @@
-#!/bin/bash
+#!/bin/bash 
 
 echo "----------GamblerGame----------"
 
+read -p "Enter how many days you want to play" days
+read -p "how many Games in day ? " games
+declare -A stakelist
+dayss
+stakes=0;
+
+for((day=1; day <= $days; day++))
+do
+	stake=100;
+	
+	for((game=1; game <= $games; game++))
+	do
+	stake=$(($stake-1))
+	echo $stake	
+	done
+
+
+
+stakes=$(($stakes+$stake))
+
+done
